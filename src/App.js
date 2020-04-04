3ff947d883727e980ab1e57908e2d22106ba72dd
Atish Agarwal <ATISHA@amdocs.com>
2020-04-04 18:27:52 +0530

    changed line

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hi Swati. I love you.. Muahhhhhhhh. Muaahhhhh.
+          Hi, I love you all.
           -From Atish
         </p>
         <a
